e6b3384e61fc7c3c200a62b100b6b0cc85285401
kevin <kevinnguetchouang@gmail.com>
2021-06-22 17:37:19 +0200

    script up

--- expes/create-vm.sh
+++ expes/create-vm.sh
@@ -15,7 +15,7 @@ virt-builder ubuntu-18.04 --format qcow2 --root-password password:a \
     --ssh-inject root:file:$PWD/keys/id_rsa.pub \
     --run-command 'sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0\"/" /etc/default/grub && update-grub' \
     --run-command 'sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/" /etc/ssh/sshd_config' \
-    --run-command 'cd /root && cp fio.job fio-tmp.job && sed -i "2icreate_only=1" fio-tmp.job && fio fio-tmp.job && rm fio-tmp.job' \
+    --run-command 'cd /root && cp fio.job fio-tmp.job && sed -i "2icreate_only=1" fio-tmp.job && fio fio-tmp.job && rm fio-tmp.job && rm job1.0.0' \
     --append-line /etc/rc.local:#!/bin/bash --append-line /etc/rc.local:dhclient \
     --chmod 777:/etc/rc.local
 
