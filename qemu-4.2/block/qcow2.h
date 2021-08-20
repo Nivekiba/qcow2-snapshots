33dafe00bf517263376359d37e8ae057e3e3fb33
kevin <kevinnguetchouang@gmail.com>
2021-08-20 10:46:27 +0200

    ras

--- qemu-4.2/block/qcow2.h
+++ qemu-4.2/block/qcow2.h
@@ -33,7 +33,7 @@
 //#define DEBUG_ALLOC
 //#define DEBUG_ALLOC2
 //#define DEBUG_EXT
-//#define DEBUG_TIME
+#define DEBUG_TIME
 #define DEBUG_TIME_FILE "time.csv"
 
 #define QCOW_MAGIC (('Q' << 24) | ('F' << 16) | ('I' << 8) | 0xfb)
